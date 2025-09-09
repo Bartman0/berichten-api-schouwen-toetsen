b747fa42bcea5bdeaa2f07fbf2a0cbed33aae2ad
Richard Kooijman <kooijman.richard@gmail.com>
2025-09-09 16:29:43 +0200

    refactoring

--- test_main.py
+++ test_main.py
@@ -20,7 +20,7 @@ API_URL_NIEUWE_INGEZETENEN = (
     f"{API_BASE_URL}/nieuwe-ingezetenen"  # Assuming a new batch endpoint
 )
 
-EINDDATUM = "2025-12-31"
+EINDDATUM_TOEKOMST = "2099-12-31"
 
 # A fixed list of person numbers to be tested against the API.
 PERSON_NUMBERS_DEEL_4_PLAATSEN = ["001", "V02"]
@@ -46,7 +46,9 @@ PERSON_NUMBERS_DEEL_7_VERWACHT = ["005", "V02"]
 
 PERSON_NUMBERS_DEEL_9_PLAATSEN = ["501", "509"]
 
-PERSON_NUMBERS_DEEL_9_VERWIJDER = ["501", "509"]
+PERSON_NUMBERS_DEEL_9_VERWIJDER = ["503"]
+
+PERSON_NUMBERS_DEEL_9_VERWACHT = ["501", "509"]
 
 
 def volgindicaties_put(bsn, einddatum, status_code_ok):
@@ -61,9 +63,8 @@ def wijzigingen_get(vanaf, burgerservicenummers_verwacht, status_code_ok):
     burgerservicenummers = set(
         request_wijzigingen_get(api_url, parameters, status_code_ok)
     )
-    assert (
-        burgerservicenummers & burgerservicenummers_verwacht
-        == burgerservicenummers_verwacht
+    assert burgerservicenummers & set(burgerservicenummers_verwacht) == set(
+        burgerservicenummers_verwacht
     )
 
 
@@ -95,10 +96,11 @@ def request_wijzigingen_get(api_url, parameters, status_code_ok):
     )
 
     try:
-        results = response.json()
+        response_json = response.json()
         burgerservicenummers = response_json.get("burgerservicenummers")
-
-        assert burgerservicenummers, "Response must contain 'burgerservicenummers'."
+        assert (
+            len(burgerservicenummers) >= 0
+        ), "Response must contain 'burgerservicenummers'."
         return burgerservicenummers
     except (json.JSONDecodeError, AssertionError) as e:
         pytest.fail(
@@ -128,8 +130,8 @@ def request_volgindicatie_put(api_url, payload, status_code_ok):
     try:
         response_json = response.json()
         einddatum = response_json.get("einddatum")
-
         assert einddatum, "Response must contain 'einddatum'."
+        return einddatum
     except (json.JSONDecodeError, AssertionError) as e:
         pytest.fail(
             f"Could not parse a valid response from the PUT request. Error: {e}"
@@ -139,7 +141,7 @@ def request_volgindicatie_put(api_url, payload, status_code_ok):
 def volgindicaties_create_plnummer(pl_lookup, pl_nummer):
     logger.debug(f"[{pl_nummer}]")
     bsn = pl_lookup[pl_nummer]
-    volgindicaties_put(bsn, einddatum=EINDDATUM, status_code_ok=201)
+    volgindicaties_put(bsn, einddatum=EINDDATUM_TOEKOMST, status_code_ok=201)
 
 
 def volgindicaties_delete_plnummer(pl_lookup, pl_nummer):
@@ -148,64 +150,76 @@ def volgindicaties_delete_plnummer(pl_lookup, pl_nummer):
     volgindicaties_put(bsn, einddatum=yesterday(), status_code_ok=200)
 
 
+def reset_volgindicaties(pl_lookup, pl_nummers):
+    for pl_nummer in pl_nummers:
+        volgindicaties_delete_plnummer(pl_lookup, pl_nummer)
+
+
+def check_verwachtingen(pl_lookup, pl_nummers):
+    burgerservicenummers = [pl_lookup[pl_nummer] for pl_nummer in pl_nummers]
+    logger.info("-" * 20)
+    logger.info(pl_nummers)
+    logger.info(burgerservicenummers)
+    logger.info("-" * 20)
+    wijzigingen_get(today(), burgerservicenummers, status_code_ok=200)
+
+
 @pytest.mark.deel_4
 def test_deel_4(pl_lookup):
-    for pl_nummer in PERSON_NUMBERS_DEEL_4:
+    pl_plaatsen = PERSON_NUMBERS_DEEL_4_PLAATSEN
+    pl_verwijderen = PERSON_NUMBERS_DEEL_4_VERWIJDER
+    setup_volgindicaties(pl_lookup, pl_plaatsen, pl_verwijderen)
+
+
+def setup_volgindicaties(pl_lookup, pl_plaatsen, pl_verwijderen):
+    for pl_nummer in pl_plaatsen:
         volgindicaties_create_plnummer(pl_lookup, pl_nummer)
-
-
-@pytest.mark.deel_4_verwijder
-def test_deel_4_verwijder(pl_lookup):
-    for pl_nummer in PERSON_NUMBERS_DEEL_4:
+    for pl_nummer in pl_verwijderen:
         volgindicaties_delete_plnummer(pl_lookup, pl_nummer)
+
+
+@pytest.mark.deel_4_reset
+def test_deel_4_reset(pl_lookup):
+    reset_volgindicaties(pl_lookup, PERSON_NUMBERS_DEEL_4_PLAATSEN)
 
 
 @pytest.mark.deel_4_verwacht
 def test_deel_4_verwacht(pl_lookup):
-    burgerservicenummers = [
-        pl_lookup[pl_nummer] for pl_nummer in PERSON_NUMBERS_DEEL_4_VERWACHT
-    ]
-    wijzigingen_get(today, burgerservicenummers, status_code_ok=200)
+    check_verwachtingen(pl_lookup, PERSON_NUMBERS_DEEL_4_VERWACHT)
 
 
 @pytest.mark.deel_7
 def test_deel_7(pl_lookup):
-    for pl_nummer in PERSON_NUMBERS_DEEL_7:
-        volgindicaties_create_plnummer(pl_lookup, pl_nummer)
+    pl_plaatsen = PERSON_NUMBERS_DEEL_7_PLAATSEN
+    pl_verwijderen = PERSON_NUMBERS_DEEL_7_VERWIJDER
+    setup_volgindicaties(pl_lookup, pl_plaatsen, pl_verwijderen)
 
 
-@pytest.mark.deel_7_verwijder
-def test_deel_7_verwijder(pl_lookup):
-    for pl_nummer in PERSON_NUMBERS_DEEL_7:
-        volgindicaties_delete_plnummer(pl_lookup, pl_nummer)
+@pytest.mark.deel_7_reset
+def test_deel_7_reset(pl_lookup):
+    reset_volgindicaties(pl_lookup, PERSON_NUMBERS_DEEL_7_PLAATSEN)
 
 
 @pytest.mark.deel_7_verwacht
 def test_deel_7_verwacht(pl_lookup):
-    burgerservicenummers = [
-        pl_lookup[pl_nummer] for pl_nummer in PERSON_NUMBERS_DEEL_7_VERWACHT
-    ]
-    wijzigingen_get(today, burgerservicenummers, status_code_ok=200)
+    check_verwachtingen(pl_lookup, PERSON_NUMBERS_DEEL_7_VERWACHT)
 
 
 @pytest.mark.deel_9
 def test_deel_9(pl_lookup):
-    for pl_nummer in PERSON_NUMBERS_DEEL_9:
-        volgindicaties_create_plnummer(pl_lookup, pl_nummer)
+    pl_plaatsen = PERSON_NUMBERS_DEEL_9_PLAATSEN
+    pl_verwijderen = PERSON_NUMBERS_DEEL_9_VERWIJDER
+    setup_volgindicaties(pl_lookup, pl_plaatsen, pl_verwijderen)
 
 
-@pytest.mark.deel_9_verwijder
-def test_deel_9_verwijder(pl_lookup):
-    for pl_nummer in PERSON_NUMBERS_DEEL_9:
-        volgindicaties_delete_plnummer(pl_lookup, pl_nummer)
+@pytest.mark.deel_9_reset
+def test_deel_9_reset(pl_lookup):
+    reset_volgindicaties(pl_lookup, PERSON_NUMBERS_DEEL_9_PLAATSEN)
 
 
 @pytest.mark.deel_9_verwacht
 def test_deel_9_verwacht(pl_lookup):
-    burgerservicenummers = [
-        pl_lookup[pl_nummer] for pl_nummer in PERSON_NUMBERS_DEEL_9_VERWACHT
-    ]
-    wijzigingen_get(today, burgerservicenummers, status_code_ok=200)
+    check_verwachtingen(pl_lookup, PERSON_NUMBERS_DEEL_9_VERWACHT)
 
 
 def main():
